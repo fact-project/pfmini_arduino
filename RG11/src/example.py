584220c8761fdb22166bdd8583d0dbdd8f63bb30
Dominik Neise <neised@phys.ethz.ch>
2017-02-02 13:07:56 +0100

    nicer output

--- RG11/src/example.py
+++ RG11/src/example.py
@@ -1,20 +1,33 @@
 import socket
 import time
-socket.setdefaulttimeout = 5.3
+import struct
+from collections import namedtuple
 
-def GET(HOST, PORT=80):
+Report = namedtuple('Report', 'dropCounter, pulse_length_in_ms, time, checksum')
+
+def request(host, port=80):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(5.30)
+    s.settimeout(5.)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # s.setblocking(0)
-    s.connect((HOST, PORT))
+    s.connect((host, port))
     s.send(b"GET / HTTP/1.0\r\n\r\n")
-    data = (s.recv(1000000))
-    # https://docs.python.org/2/howto/sockets.html#disconnecting
+    data = s.recv(1000000)
     s.shutdown(1)
     s.close()
     return data
 
 while True:
-    print(time.asctime(), GET('10.0.100.155'))
+    try:
+        msg = struct.unpack("IIIH", request('10.0.100.155'))
+        report = Report(
+            dropCounter=msg[0],
+            pulse_length_in_ms=msg[1] / 1e3,  # 1e3 convert us to ms.
+            time=msg[2] / 1e3,  # 1e3 convert ms to seconds.
+            checksum=msg[3]
+        )
+
+        print(time.asctime(), report)
+    except Exception as e:
+        print(time.asctime(), e)
+
     time.sleep(1)
