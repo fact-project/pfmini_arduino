5ea6855992f7e5f78d54f09f3162ec1a4d660e84
Dominik Neise <neised@phys.ethz.ch>
2017-02-06 10:21:03 +0100

    adjust to 2nd RG11

--- RG11/src/example.py
+++ RG11/src/example.py
@@ -3,7 +3,15 @@ import time
 import struct
 from collections import namedtuple
 
-Report = namedtuple('Report', 'dropCounter, pulse_length_in_ms, time, checksum')
+Report = namedtuple('Report',
+    ['drop_counter_number_of_pulses',
+    'drop_counter_pulse_length',
+    'condensation_detector_number_of_pulses',
+    'condensation_detector_pulse_length',
+    'time_since_boot_in_ms',
+    'time_between_message_updates_in_ms',
+    ]
+)
 
 def request(host, port=80):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,13 +27,8 @@ def request(host, port=80):
 if __name__ == "__main__":
     while True:
         try:
-            msg = struct.unpack("IIIH", request('10.0.100.155'))
-            report = Report(
-                dropCounter=msg[0],
-                pulse_length_in_ms=msg[1] / 1e3,  # 1e3 convert us to ms.
-                time=msg[2] / 1e3,  # 1e3 convert ms to seconds.
-                checksum=msg[3]
-            )
+            msg = struct.unpack("IIIIIIH", request('192.168.0.110'))
+            report = Report(*msg[:6])
 
             print(time.asctime(), report)
         except Exception as e:
